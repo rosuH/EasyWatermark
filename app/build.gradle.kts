49dc9fa19d6b4a8da5e2fb83f3d872f6eabad6aa
rosuH <rosuh@qq.com>
2021-01-27 11:27:05 +0800

    🐛
    Fix typo for F-Droid building CI.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "me.rosuh.easywatermark"
         minSdkVersion(Apps.minSdk)
         targetSdkVersion(Apps.targetSdk)
-        versionCode = Apps.versionCode
-        versionName = Apps.versionName
+        versionCode = 10102
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
