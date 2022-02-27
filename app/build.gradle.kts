19645a70e06bae70cf0fd2d0402ece588758de60
rosuh <hi@rosuh.me>
2022-02-27 17:41:23 +0800

    :bookmark: :lipstick: - Bump up the version - Update screen shot files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.rosuh.easywatermark"
         minSdk = (Apps.minSdk)
         targetSdk = (Apps.targetSdk)
-        versionCode = 20605
-        versionName = "2.6.5"
+        versionCode = 20606
+        versionName = "2.6.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
     }
