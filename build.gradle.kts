1872756e0e0562e3ba55b907c51653c0a13d941d
rosuH <hi@rosuh.me>
2022-09-11 12:08:48 +0800

    :arrow_up:

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.3.0-rc01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
