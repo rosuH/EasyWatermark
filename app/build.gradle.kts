70f634b27027934d651c0d415c2543dbf93aeac8
rosuH <hi@rosuh.me>
2022-12-18 13:01:25 +0800

    :bookmark:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.rosuh.easywatermark"
         minSdk = (Apps.minSdk)
         targetSdk = (Apps.targetSdk)
-        versionCode = 20806
-        versionName = "2.8.6"
+        versionCode = 20807
+        versionName = "2.8.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
     }
