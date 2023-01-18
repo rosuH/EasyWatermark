83001a2753a6d163332a8ef98e49e98c0138ef57
rosuH <hi@rosuh.me>
2023-01-18 16:17:09 +0800

    :zap:

--- app/src/main/java/me/rosuh/easywatermark/utils/bitmap/BitmapCache.kt
+++ app/src/main/java/me/rosuh/easywatermark/utils/bitmap/BitmapCache.kt
@@ -9,22 +9,19 @@ import android.util.LruCache
  * @author hi@rosuh.me
  */
 object BitmapCache {
-
     private val memoryCache: LruCache<BitmapInfo, BitmapValue> by lazy {
         object : LruCache<BitmapInfo, BitmapValue>(cacheSize) {
             override fun sizeOf(key: BitmapInfo?, value: BitmapValue?): Int {
                 return if (value?.bitmap == null) {
                     super.sizeOf(key, value)
                 } else {
-                    value.bitmap.byteCount / 1024
+                    value.bitmap.allocationByteCount / 1024
                 }
             }
         }
     }
 
-    private val maxMemory by lazy {
-        (Runtime.getRuntime().maxMemory() / 1024).toInt()
-    }
+    private val maxMemory by lazy { (Runtime.getRuntime().maxMemory() / 1024).toInt() }
 
     val cacheSize = maxMemory / 8
 
