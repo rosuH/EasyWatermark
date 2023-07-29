7e2eeed7855e08c8013328cb0499d27667770be3
rosuH <hi@rosuh.me>
2023-07-29 22:58:57 +0800

    Update versionCode and versionName in build.gradle.kts to 20809 and "2.8.9" respectively.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.rosuh.easywatermark"
         minSdk = (Apps.minSdk)
         targetSdk = (Apps.targetSdk)
-        versionCode = 20808
-        versionName = "2.8.8"
+        versionCode = 20809
+        versionName = "2.8.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
     }
