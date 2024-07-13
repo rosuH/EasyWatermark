15f59113ccdff56c05d4601de058be74b1b87701
rosuH <hi@rosuh.me>
2024-07-13 08:15:27 +0800

    chore(deps): update buildTools and targetSdk to version 34.0.0
    # GENERATE BY https://aicommit.app

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Apps {
     const val compileSdk = 34
-    const val buildTools = "33.0.0"
+    const val buildTools = "34.0.0"
     const val minSdk = 23
-    const val targetSdk = 33
+    const val targetSdk = 34
 }
