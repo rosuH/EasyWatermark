d3427acae60eb5ae3574f7e4239c0256eaf42063
rosuH <hi@rosuh.me>
2025-08-30 10:12:00 +0800

    Merge branch 'master' into feat/migrate_to_compose
    # Conflicts:
    #	app/src/main/java/me/rosuh/easywatermark/ui/MainActivity.kt
    #	app/src/main/java/me/rosuh/easywatermark/utils/ktx/ContextExtension.kt
    #	gradle.properties

--- app/src/main/java/me/rosuh/easywatermark/ui/MainActivity.kt
+++ app/src/main/java/me/rosuh/easywatermark/ui/MainActivity.kt
@@ -243,7 +243,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         this.intent = intent
     }
