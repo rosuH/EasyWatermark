c751a5b6956835b0a3e1aa2dec690ccc88a4a110
rosuH <hi@rosuh.me>
2025-10-26 14:27:20 +0800

    fix(context): update storage permission for Android 13+
    - Replaced READ_EXTERNAL_STORAGE with conditional permission logic
      to support Android 13 (TIRAMISU) and above.
    - Introduced permission variable for backward compatibility.
    - Ensured consistent call to preCheckStoragePermission().
    - Modified ContextExtension.kt to handle new Android SDK changes.

--- app/src/main/java/me/rosuh/easywatermark/utils/ktx/ContextExtension.kt
+++ app/src/main/java/me/rosuh/easywatermark/utils/ktx/ContextExtension.kt
@@ -31,11 +31,12 @@ fun Activity.checkReadingPermission(
     },
     grant: () -> Unit,
 ) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        grant.invoke()
-        return
+    val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        Manifest.permission.READ_MEDIA_IMAGES
+    } else {
+        Manifest.permission.READ_EXTERNAL_STORAGE
     }
-    preCheckStoragePermission(activityResultLauncher, Manifest.permission.READ_EXTERNAL_STORAGE, failed, grant)
+    preCheckStoragePermission(activityResultLauncher, permission, failed, grant)
 }
 
 fun Activity.checkWritingPermission(
